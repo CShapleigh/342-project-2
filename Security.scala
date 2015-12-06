a1ab2a95f8ef0952d6722487bd71aff8932de89d
unceldolan <sed9182@rit.edu>
2015-12-06 17:10:51 -0500

    Security first commit

---
+++ Security.scala
@@ -0,0 +1,46 @@
+import akka.actor.{Actor, ActorRef}
+import akka.event.Logging
+
+import scala.collection.mutable
+
+class Security(jail: ActorRef) extends Actor {
+  val log = Logging(context.system, this)
+  val   baggageReports = mutable.ArrayBuffer[Boolean]()
+  val   personReports = mutable.ArrayBuffer[Boolean]()
+  val   PeopleWaiting = mutable.ArrayBuffer[ActorRef]()
+  def receive = {
+    case BodyReport(person, status) =>
+      if(person == PeopleWaiting.head){
+        guiltyorNot()
+      }
+      else{
+        PeopleWaiting.append(person)
+        personReports.append(status)
+      }
+
+    case BagReport(person, status) =>
+      if(person == PeopleWaiting.head){
+        guiltyorNot()
+      }
+      else{
+        PeopleWaiting.append(person)
+        baggageReports.append(status)
+      }
+  }
+  def guiltyorNot() = {
+    if (baggageReports.head && personReports.head){
+      log.info("Go directly to the plane")
+      PeopleWaiting -= PeopleWaiting.head
+      personReports -= personReports.head
+      baggageReports -= baggageReports.head
+
+    }
+    else{
+      jail ! PersonGoingToJail(PeopleWaiting.head)
+      log.info("Go directly to jail")
+      PeopleWaiting -= PeopleWaiting.head
+      personReports -= personReports.head
+      baggageReports -= baggageReports.head
+    }
+  }
+}
