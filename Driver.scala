5f46fbca0cbdb789a580bcf0ea9fc931e79cc9a1
Charles Shapleigh <CShapleigh@gmail.com>
2015-12-06 14:20:33 -0500

    Fixed driver

--- Driver.scala
+++ Driver.scala
@@ -8,15 +8,18 @@ object Driver extends App {
     val tsaScreenSystem = ActorSystem("TSA")
     val jail = system.actorOf(Props[Jail], name = "jail")
     val plane = system.actorOf(Props[Gate], name = "plane")
+    val securityGuy = system.actorOf(Props[Security], name = "joe")
 
     val queues: Array[ActorRef] = new Array(queueCount)
     val documentCheck = system.actorOf(Props(new DocumentCheck(queueCount)), name = "documentCheck")
 
     for (i <- 0 util queueCount) {
+      val tempBodyScan = system.actorOf(Props(new BodyScan(i, securityGuy)), name = "bodyScan " + i)
+      val tempBaggageScan = system.actorOf(Props(new BaggageCheck(i, securityGuy)), name = "bodyScan " + i)
       queues(i) = system.actorOf(Props(new Queue(i)), name = "queue number: " + i)
     }
 
     for (i <- 0 util personCount) {
-      val person = system.actorOf(Props(new Person()), name = "person: " + i)
+      val person = system.actorOf(Props(new Person(i, documentCheck, queues)), name = "person: " + i)
     }
 }
